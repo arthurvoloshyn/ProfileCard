3758d882da3dc4df1156ce0c1d6cb9eda4827fb1
Artur <grandfaraon93@gmail.com>
2019-12-09 18:30:30 +0200

    Updates

---
+++ precache-manifest.c12f11cb00641def89f2c85640dd6009.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9d881860f6a6e7140fc9357f9f3a4fb",
+    "url": "/ProfileCard/index.html"
+  },
+  {
+    "revision": "c2ce4cb5a03455015e24",
+    "url": "/ProfileCard/static/css/main.eedba485.chunk.css"
+  },
+  {
+    "revision": "e561937fb3f755685962",
+    "url": "/ProfileCard/static/js/2.46a66ed7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ProfileCard/static/js/2.46a66ed7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c2ce4cb5a03455015e24",
+    "url": "/ProfileCard/static/js/main.108b6e7c.chunk.js"
+  },
+  {
+    "revision": "e45058ef6e1421230ff1",
+    "url": "/ProfileCard/static/js/runtime-main.8d0dd960.js"
+  }
+]);
