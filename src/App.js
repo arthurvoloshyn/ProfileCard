f400002d6e5343df84fb6a3b32bee66b4180d944
arturvoloshyn <arturvoloshyn@gmail.com>
2021-11-15 01:51:47 +0200

    Remove the header component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from 'react';
 
 import CardProfile from './containers/CardProfile';
-import Header from './components/Header';
 
 import './style/App.css';
 
-const App = () => (
-  <>
-    <Header />
-    <CardProfile />
-  </>
-);
+const App = () => <CardProfile />;
 
 export default App;
